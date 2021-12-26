17574557b8127c1583432441a26894a6a6f6f160
sumit kumar <sumitdelin@gmail.com>
2021-12-26 21:40:37 +0530

    EmployeeDoa commit

--- src/dao/EmployeeDoa.java
+++ src/dao/EmployeeDoa.java
@@ -5,7 +5,9 @@ import bean.Employee;
 public interface EmployeeDoa {
     
     int saveEmployee(Employee e);
+    
     int deleteEmployee(Employee e);
+    
     int updateEmployee(Employee e);
     Employee getEmployee(int id);
     int getMaxEmpSalary();
